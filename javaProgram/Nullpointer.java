d6a4cfc5b9b8c8c3833c519dd6e9de76deb0b5e8
HARIHARAN J <155130990+Hariharankrishna@users.noreply.github.com>
2025-06-20 11:31:13 +0530

    Add files via upload

---
+++ javaProgram/Nullpointer.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+class Nullpointer {
+    public static void main(String[] args) {
+        Integer[] arr = {12, 11, 15, null, 12, 13};
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a number to divide: ");
+            int num = sc.nextInt();
+            for (int i = 0; i < arr.length; i++) {
+                try {
+                    int result = num / arr[i];
+                    System.out.println(num + " / " + arr[i] + " = " + result);
+                } catch (NullPointerException e) {
+                    System.out.println(e);
+            }
+        }
+    }
+}
