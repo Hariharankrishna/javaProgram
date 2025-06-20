35a1c2fe993cb4680b5c3ad659f7f5cf51728d7d
HARIHARAN J <155130990+Hariharankrishna@users.noreply.github.com>
2025-06-20 10:28:02 +0530

    Add files via upload

---
+++ javaProgram/trycatchexception.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class trycatchexception {
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 15, 0, 12, 13};
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a number to divide: ");
+            int num = sc.nextInt();
+            for (int i = 0; i < arr.length; i++) {
+                try {
+                    int result = num / arr[i];
+                    System.out.println(num + " / " + arr[i] + " = " + result);
+                } catch (ArithmeticException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+
